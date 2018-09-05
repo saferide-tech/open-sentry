b7d03fdbc21954e70c0389c2d3eeb70da3222ea1
Arik Alexander <you@example.com>
2018-09-05 00:53:24 -0700

    Add file permissions defines.

--- libsentry/inc/file_rule.h
+++ libsentry/inc/file_rule.h
@@ -10,6 +10,10 @@
 #define FILE_TUPLES_XPATH_FORMAT   "/saferide:config/system/file/rule[num='%d']/tuple"
 #define FILE_TUPLEID_XPATH_FORMAT  "/saferide:config/system/file/rule[num='%d']/tuple[id='%d']"
 
+#define FILE_PERM_R (1 << 2)
+#define FILE_PERM_W (1 << 1)
+#define FILE_PERM_X (1 << 0)
+
 typedef struct {
     unsigned int    id;
     char            filename[FILE_NAME_SIZE];
